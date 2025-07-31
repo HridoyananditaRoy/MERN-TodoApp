f5023c8a6be41618836f0c155cb09cfde4a745e1
Hridoyanandita Roy <161694723+HridoyananditaRoy@users.noreply.github.com>
2025-07-31 22:14:49 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,10 @@ const userRoutes = require('./routes/user.routes.js')
 const connectDB = require('./database/db');
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://mern-todoapp-frontend-2bnw.onrender.com'],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/v1/user', userRoutes); 
